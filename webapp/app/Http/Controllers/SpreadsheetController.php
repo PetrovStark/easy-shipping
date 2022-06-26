c5305dc7ddaedee71cc4d7177f3bffc7ded636c0
Petrus Nogueira <petrusyagami@gmail.com>
2022-06-26 16:24:10 -0300

    feat: create Spreadsheet MVC

---
+++ webapp/app/Http/Controllers/SpreadsheetController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SpreadsheetController extends Controller
+{
+    public function upload()
+    {
+        dd('Upload code goes here!');
+    }
+}
