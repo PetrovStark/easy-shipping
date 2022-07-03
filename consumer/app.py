b0db966d0bcf1a7339cba5a5f3403e19eb624be7
Petrus Nogueira <petrusyagami@gmail.com>
2022-07-03 16:57:54 -0300

    feat: try connect with broker

--- consumer/app.py
+++ consumer/app.py
@@ -4,13 +4,12 @@ import os
 
 def main():
     while True:
-        print('Pringles')
-    # connection = pika.BlockingConnection(pika.ConnectionParameters('messagebroker', 5672))
-    # channel = connection.channel()
-    # channel.queue_declare(queue='csv_paths')
+        connection = pika.BlockingConnection(pika.ConnectionParameters('messagebroker', 5672))
+        channel = connection.channel()
+        channel.queue_declare(queue='csv_paths')
 
-    # channel.basic_consume(queue='csv_paths', on_message_callback=import_csv, auto_ack=True)
-    # channel.start_consuming()
+        channel.basic_consume(queue='csv_paths', on_message_callback=import_csv, auto_ack=True)
+        channel.start_consuming()
 
 def import_csv(ch, method, properties, body):
     """Spreadsheets import script"""
